b556a025f81cc8e52d86f3b54385affb40bd813a
Alek Westover <alek.westover@gmail.com>
2017-07-22 23:18:26 -0400

    Add files via upload

--- diffusion/particle.js
+++ diffusion/particle.js
@@ -4,9 +4,10 @@
 function Particle(pos, vel, dims, type, max_speed) {
 	this.pos = pos;
 	this.vel = vel;
-	this.dims = dims;
+	this.dims = dims;  //[width, height] (for circle  [diameter, diameter])
 	this.type = type;
 	this.max_speed = max_speed;
+	this.random_vel_needed = true;
 };
 
 // displays the particles
@@ -21,34 +22,49 @@ Particle.prototype.show = function() {
 	}
 };
 
-// gives particels random velocities and updates their positions
+// updates their positions
 Particle.prototype.update = function(dt) {
 	this.pos.add(p5.Vector.mult(this.vel, dt));
-	this.vel = p5.Vector.random2D(); 	
+	this.random_vel_needed = true;
 };
+
+// asigns a random velocity to each particle as the default motion
+Particle.prototype.set_random_vel = function() {
+	if (this.random_vel_needed) {
+		this.vel = p5.Vector.random2D().mult(0.7*this.max_speed); 
+	}
+}
 
 // makes particles bounce off of the walls
 Particle.prototype.handle_wall_collide = function(screen_dims) {
-	if (this.pos.x <= 0) {
+	if (this.pos.x - this.dims[0]*0.5 <= 0) {
 		this.vel.x = this.max_speed;
+		this.random_vel_needed = false;
 	} 
-	else if (this.pos.x >= screen_dims[0]) {
+	else if (this.pos.x + this.dims[0]*0.5 >= screen_dims[0]) {
 		this.vel.x = -this.max_speed;
+		this.random_vel_needed = false;
 	}
-	if (this.pos.y >= screen_dims[1]) {
+	if (this.pos.y + this.dims[1]*0.5 >= screen_dims[1]) {
 		this.vel.y = -this.max_speed;
+		this.random_vel_needed = false;
 	}
-	else if (this.pos.y <= 0) {
+	else if (this.pos.y - this.dims[1]*0.5 <= 0) {
 		this.vel.y = this.max_speed;
+		this.random_vel_needed = false;
 	}
 };
 
-// handles membrane collision accepting or rejecting particles based on their type
-Particle.prototype.hit_membrane = function(membrane_pos, membrane_dims) {
-	if (this.type == "allowed") {
-		console.log("CARRY ON");
-	}
-	else {
-		console.log("YOU SHALL NOT PASS");
+// handles non wall boundary collisions accepting or rejecting particles based on their type
+Particle.prototype.hit_boundary = function(boundary_pos, boundary_dims, allowed_through) {
+	if (this.type != allowed_through) {
+		var x_right = this.pos.x + this.dims[0] * 0.5 >= boundary_pos[0];
+		var x_left = this.pos.x - this.dims[0] * 0.5 <= boundary_pos[0] + boundary_dims[0];
+		var y_bottom = this.pos.y + this.dims[1] * 0.5 >= boundary_pos[1];
+		var y_top = this.pos.y - this.dims[1] * 0.5 <= boundary_pos[1] + boundary_dims[1];
+		if (x_right && x_left && y_top && y_bottom) {
+			this.vel.mult(-1);
+			this.random_vel_needed = false;
+		}
 	}
 };
--- diffusion/sketch.js
+++ diffusion/sketch.js
@@ -3,32 +3,80 @@
 
 // variables initialized
 var screen_dims = [500, 500];
-var screen_color = [0, 0, 0];
-var particles = [];
-var num_particles = [10, 10]; // type a, type b
-var dt = 1;
+var channel_pos = [0.4*screen_dims[0], 0.4*screen_dims[1]];
+var channel_dims = [0.2*screen_dims[0], 0.2*screen_dims[1]];
+var boundary_bands_loc = [[0, 0.45*screen_dims[1]], [0.6*screen_dims[0], 0.45*screen_dims[1]]]; //impassable strips on either side of the channel
+var boundary_bands_dims = [[0.4*screen_dims[0], 0.1*screen_dims[1]], [0.4*screen_dims[0], 0.1*screen_dims[1]]];
+var screen_color = [0, 0, 100];
 
+var particles;
+var num_particles = [10, 10]; // type allowed through channel, type not allowed through channel
+var max_speed;
+
+var dt;
+
+
+// resets all of the particels, initializes the pariticles array to empty set and then repopulates it
+function reset_sim () {
+	//initialize variables
+	particles = [];
+	max_speed = 0.05;
+	dt = 30;
+	// creates the allowed particles
+	for (var i = 0; i < num_particles[0]; i++) {
+		particles.push(new Particle(createVector(random()*screen_dims[0], random()*channel_pos[1]), p5.Vector.random2D(), [10, 10], 'allowed', max_speed));
+	}
+	// Create the not allowed particles
+	for (var i = 0; i < num_particles[1]; i++) {
+		particles.push(new Particle(createVector(random()*screen_dims[0], random()*channel_pos[1]), p5.Vector.random2D(), [7, 7], 'not allowed', max_speed));
+	}
+}
+
+// handles the input tags, makes sure calid integer input is given
+function env_changes() {
+	if (!isNaN(dt)) {
+		dt = parseInt(document.getElementById('dt').value);
+	}
+	if (!isNaN(max_speed)) {
+		max_speed = parseInt(document.getElementById('heat').value);
+	}
+	for (var i = 0; i < pariticles.length; i++) {
+		particles[i].max_speed = max_speed;
+	}
+}
 
 // setup function
 function setup() {
 	createCanvas(screen_dims[0], screen_dims[1]);
-	for (var i = 0; i < num_particles[0]; i++) {
-		particles.push(new Particle(p5.Vector.random2D(), p5.Vector.random2D(), [10, 10], 'allowed', 10));
-	}
-	for (var i = 0; i < num_particles[1]; i++) {
-		particles.push(new Particle(p5.Vector.random2D(), p5.Vector.random2D(), [7, 7], 'not allowed', 10));
-	}
-
+	// Make all particles
+	reset_sim();
 }
 
 // main loop
 function draw() {
+	frameRate(dt);
+	// background color for simulation window
 	background(screen_color[0], screen_color[1], screen_color[2]);
+	// Draw the boundaries that nothing is let through
+	fill(70, 70, 70);
+	rect(boundary_bands_loc[0][0], boundary_bands_loc[0][1], boundary_bands_dims[0][0], boundary_bands_dims[0][1]);
+	rect(boundary_bands_loc[1][0], boundary_bands_loc[1][1], boundary_bands_dims[1][0], boundary_bands_dims[1][1]);
+	//Draw the channel
+	fill(30, 30, 30);
+	rect(channel_pos[0], channel_pos[1], channel_dims[0], channel_dims[1]);
+
 	for (var i = 0; i < particles.length; i++) {
+		// show and update and check boundaries for all particles
 		particles[i].show();
-		particles[i].update(dt);
 		particles[i].handle_wall_collide(screen_dims);
-		//particles[i].hit_membrane(pos, dims);
+		particles[i].hit_boundary(boundary_bands_loc[0], boundary_bands_dims[0], 'no one');
+		particles[i].hit_boundary(boundary_bands_loc[1], boundary_bands_dims[1], 'no one');
+		particles[i].hit_boundary(channel_pos, channel_dims, 'allowed');
+		//only sets random velocity if velocity hasn't changed yet this round
+		particles[i].set_random_vel();
+		particles[i].update(dt);
 	}
 
 }
+
+
