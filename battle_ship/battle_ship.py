57a309f405d5317c67787606630d6e77b4c9f808
Alek Westover <alek.westover@gmail.com>
2017-10-14 18:49:12 -0400

    Update battle_ship.py

--- battle_ship/battle_ship.py
+++ battle_ship/battle_ship.py
@@ -3,17 +3,20 @@ from battle_ship_functions import *
 from pygame.locals import *
 import pygame
 
-
-# Player view of hits/misses,
-# player view of their own board,
-# computer view of hits and misses,
-# computer view of own board
+"""
+Player view of hits/misses,
+Player view of their own board,
+Computer view of hits and misses,
+Computer view of own board
+"""
 boards = [[], [], [], []]
 n = 10
-
 """
-Notes
-H represents a hit M represents a miss, N represents Null, S represents ship pressence
+H represents a hit,
+M represents a miss,
+N represents Null,
+S represents ship pressence,
+A stands for proposed ship location, (will be checked and, if it is placed in a valid location, converted to an 'S')
 """
 
 # initialize board
@@ -29,61 +32,68 @@ for i in range(0, 5):
 for i in range(0, 5):
     place_ship(boards, "player", [0, i])
 
-
 # set up pygame
 pygame.init()
-
-# set up the window
+# set up the window parameters
 screenDims = (500, 500)
-
 width = int(screenDims[0]/n)
 height = int(screenDims[1]/n)
-
-mouseDown = False
-
+cur_display_board = 0
 windowSurface = pygame.display.set_mode(screenDims, 0, 32)
 pygame.display.set_caption("Intense Battle Ship Game")
 
-
-
-print("Welcome to the intenseest game of battle ship you will ever play ")
+# basic parameters
+mouseDown = False
+setup = False
 playing = True
 turn = "player"
 
+# initial welcome
+print("Welcome to the intenseest game of battle ship you will ever play ")
+update_board(n, windowSurface, width, height, 1, boards[cur_display_board])
 
-update_board(n, windowSurface, width, height, 1, boards[0])
 
 # run the game loop
 while playing:
-    # WARNING: this will be run through more than once per round
-
+    # check events
     for event in pygame.event.get():
         if event.type == QUIT:
             exit_game()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_e:
                 exit_game()
+            elif event.key == pygame.K_s:
+                cur_display_board = (cur_display_board + 1) % 4
+                update_board(n, windowSurface, width, height, 1, boards[cur_display_board])
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouseDown = True
         elif event.type == pygame.MOUSEBUTTONUP:
             mouseDown = False
 
-    if turn == "player":
-        validMove = False
-        if mouseDown:
-            print_boards(boards, n, turn)
-            validMove = player_move(boards, width, height)
-        if validMove != False:
-            turn = "computer"
-            update_board(n, windowSurface, width, height, 1, boards[0])
-            print("Computer take a guess")
+    if setup:
 
-    else:
-        print_boards(boards, n, turn)
-        computer_move(boards)
-        turn = "player"
-        update_board(n, windowSurface, width, height, 1, boards[0])
-        print("Player make a guess")
+        if turn == "player":
+            validMove = False
+            if mouseDown:
+                print_boards(boards, n, turn)
+                validMove = player_move(boards, width, height)
+            if validMove != False:
+                turn = "computer"
+                update_board(n, windowSurface, width, height, 1, boards[cur_display_board])
+                print("Computer take a guess")
+
+        elif turn == "computer":
+            print_boards(boards, n, turn)
+            computer_move(boards)
+            turn = "player"
+            update_board(n, windowSurface, width, height, 1, boards[cur_display_board])
+            print("Player make a guess")
+
+    elif not setup:
+        print("Please place your pieces by clicking on the squares on which you want to place ships")
+
+
+
 
 
 
