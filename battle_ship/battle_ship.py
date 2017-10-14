2188b632cd8891e0a40573f65b6588fd0140b675
Alek Westover <alek.westover@gmail.com>
2017-10-14 19:33:28 -0400

    Update battle_ship.py

--- battle_ship/battle_ship.py
+++ battle_ship/battle_ship.py
@@ -34,23 +34,34 @@ for i in range(0, 5):
 
 # set up pygame
 pygame.init()
+
+# resets the variables which depend on screenDims
+def resize_dims(newScreenDims):
+    screenDims = newScreenDims
+    width = int(screenDims[0]/n)
+    height = int(screenDims[1]/n)
+    return (width, height, screenDims)
+
 # set up the window parameters
-screenDims = (500, 500)
-width = int(screenDims[0]/n)
-height = int(screenDims[1]/n)
+width, height, screenDims = resize_dims((500, 500))
 cur_display_board = 0
-windowSurface = pygame.display.set_mode(screenDims, 0, 32)
-pygame.display.set_caption("Intense Battle Ship Game")
+windowSurface = pygame.display.set_mode(screenDims, RESIZABLE)
 
 # basic parameters
 mouseDown = False
-setup = False
+already_setup = False
 playing = True
 turn = "player"
 
+# draws the board without having to input the globals
+def draw_board_globals():
+    update_board(n, windowSurface, width, height, 1, boards[cur_display_board], cur_display_board)
+
 # initial welcome
 print("Welcome to the intenseest game of battle ship you will ever play ")
-update_board(n, windowSurface, width, height, 1, boards[cur_display_board])
+pygame.display.set_caption("Intense Battle Ship Game")
+draw_board_globals()
+print("Please place your pieces by clicking on the squares on which you want to place ships")
 
 
 # run the game loop
@@ -59,18 +70,30 @@ while playing:
     for event in pygame.event.get():
         if event.type == QUIT:
             exit_game()
+        # key presses
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_e:
                 exit_game()
             elif event.key == pygame.K_s:
                 cur_display_board = (cur_display_board + 1) % 4
-                update_board(n, windowSurface, width, height, 1, boards[cur_display_board])
+                draw_board_globals()
+            elif event.key == pygame.K_d:
+                # TODO: check to make sure ship stuff is valid and stuff
+                already_setup = True
+
+        # mouse interaction
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouseDown = True
         elif event.type == pygame.MOUSEBUTTONUP:
             mouseDown = False
 
-    if setup:
+        # resize capability
+        elif event.type==VIDEORESIZE:
+            width, height, screenDims = resize_dims(event.dict['size'])
+            windowSurface = pygame.display.set_mode(screenDims, RESIZABLE)
+            draw_board_globals()
+
+    if already_setup:
 
         if turn == "player":
             validMove = False
@@ -79,25 +102,21 @@ while playing:
                 validMove = player_move(boards, width, height)
             if validMove != False:
                 turn = "computer"
-                update_board(n, windowSurface, width, height, 1, boards[cur_display_board])
+                draw_board_globals()
                 print("Computer take a guess")
 
         elif turn == "computer":
             print_boards(boards, n, turn)
             computer_move(boards)
             turn = "player"
-            update_board(n, windowSurface, width, height, 1, boards[cur_display_board])
+            draw_board_globals()
             print("Player make a guess")
 
-    elif not setup:
-        print("Please place your pieces by clicking on the squares on which you want to place ships")
+    elif not already_setup:
 
-
-
-
-
-
-
+        if mouseDown:
+            place_ship(boards, "player", get_clicked_box(width, height), tentative=True)
+            draw_board_globals()
 
 
 
